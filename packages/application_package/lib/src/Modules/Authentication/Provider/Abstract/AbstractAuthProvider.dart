18b7cd9459ce9de77ca4c1c267eb3e6ea13d0354
Philac35 <philac35@gmail.com>
2025-08-18 10:08:16 +0200

    EcodriverServer [Wip] Move Authenticator module (non flutter part) 18/08/2025 10h08

--- packages/application_package/lib/src/Modules/Authentication/Provider/Abstract/AbstractAuthProvider.dart
+++
@@ -1,8 +0,0 @@
-// Abstract AuthProvider
-import '../../Entities/AuthUser.dart';
-
-abstract interface class AbstractAuthProvider {
-  Future<void> connect(String email, String password);
-  Future<void> disconnect();
-  AuthUser? get currentUser;
-}
